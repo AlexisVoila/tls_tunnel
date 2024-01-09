d4bd08f2f7b02bb13532ab02f80abf5e779cb657
Alexis Voila <alexisvoila@gmail.com>
2024-01-09 13:16:21 +0300

    Added output of information about the operating mode.

--- src/tls_tunnel/tunnel.rs
+++ src/tls_tunnel/tunnel.rs
@@ -6,8 +6,10 @@ use std::{io};
 
 pub async fn run(cfg: config::TlsTunnelConfig) -> io::Result<()> {
     if cfg.ca_cert.is_some() {
+        println!("Working in tls tunnel mode.");
         tls_tunnel::run(cfg).await?;
     } else {
+        println!("Working in tcp port forwarding mode.");
         forwarder::run(cfg).await?;
     }
     Ok(())
